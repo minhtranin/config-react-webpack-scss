5907adbe39f02528e4a97e6f522be82b8112a1e9
minhtranin <minhtran.in@outlook.com.com>
2020-02-08 12:45:07 -0500

    docs

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,17 @@ const config = {
         filename: '[name].js',
         path: path.resolve(__dirname, 'build')
     },
+    optimization: { // cache bundle & vendor
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendor',
+                    chunks: 'all'
+                }
+            }
+        }
+    },
     module: {
         rules: [
             {
@@ -51,6 +62,22 @@ const config = {
         ]
     },
     devServer,
+    // resolve: {
+    //     modules: [
+    //         resolve('app'),
+    //         resolve('app/css'),
+    //         'node_modules'
+    //     ],
+
+    //     alias: {
+    //         // external libraries
+    //         jquerynotify: resolve('app/js/jquery.notify.min'),
+    //         clipboard: resolve('app/js/clipboard.min'),
+
+    //         // directory alias to shorten template paths
+    //         templates: resolve('app/templates')
+    //     }
+    // },
     plugins: [
         new HtmlWebPack({
             template: 'src/index.html'
@@ -67,3 +94,12 @@ const config = {
                         // ]
 }
 module.exports = config
+// "babel-core": "^6.26.3",//get code- analyze -> 1 file
+// "babel-loader": "^7.1.5",// how to work with webpack
+// "babel-preset-env": "^1.7.0", // transform es67 -> es5
+// "babel-preset-react": "^6.24.1",// transform jsx -> js
+// "css-loader": "^3.4.2",// allow webpack understand syntax import css from entry
+// "extract-text-webpack-plugin": "^4.0.0-beta.0", // detach 1 file external
+// "node-sass": "^4.13.1",// scss, sass load
+// "sass-loader": "^8.0.2",// scss, sass load
+// "style-loader": "^1.1.3",// add css to html
